0db7b39abe94f82034e27827263b3db1a8284b53
akudo37 <akira.kudo7777@gmail.com>
2019-01-18 18:02:57 +0900

    first code complete

--- app/run.py
+++ app/run.py
@@ -10,11 +10,12 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
-
+from ..models.train_classfier import tokenize  # using my own tokenizer
 
 app = Flask(__name__)
 
-def tokenize(text):
+# replaced tokenizer with my own (imported)
+'''def tokenize(text):
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -24,25 +25,26 @@ def tokenize(text):
         clean_tokens.append(clean_tok)
 
     return clean_tokens
+'''
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('response', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -65,11 +67,11 @@ def index():
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,13 +80,13 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,187 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ML pipeline that trains classifier and saves
+"""
+# import libraries
 import sys
-
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report, precision_recall_fscore_support, accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.preprocessing import MinMaxScaler
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+import re
+import pickle
+from text_len_extractor import TextLengthExtractor
 
 def load_data(database_filepath):
-    pass
+    '''Load cleaned data from database into dataframe.
+
+    Parameters
+    ----------
+    database_filepath : string
+        path to loading database(db) file
+
+    Returns
+    -------
+    X: dataframe in shape = [n_samples, n_features]
+        The input samples.
+
+    Y: dataframe in shape = [n_samples, n_outputs]
+        The target values.
+
+    category_names: list in shape = [n_outputs,]
+        The column names of target values.
+    '''
+
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('response', engine)
+    X = df['message']
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+
+    # extract category names
+    category_names = Y.columns.values
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    '''Tokenize text.
+
+    Replace URL, case normalize, tokenize, remove stop words, and lemmatize.
+
+    Parameters
+    ----------
+    text : string
+        text string to be tokenized
+
+    Returns
+    -------
+    tokenized words: list
+    '''
+
+    # replace URL with placeholder
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    urls = re.findall(url_regex, text)
+    for url in urls:
+        text = text.replace(url, 'urlplaceholder')
+
+    # case normalize, clean, and tokenize
+    text = re.sub(r'[^0-9a-zA-Z]', ' ', text.lower())
+    words = text.split()
+
+    # remove stop words
+    stop_removed = [
+            word for word in words if word not in stopwords.words('english')]
+
+    # lemmatize (nouns then verbs)
+    n_lemmed = [WordNetLemmatizer().lemmatize(word) for word in stop_removed]
+
+    n_v_lemmed = [
+            WordNetLemmatizer().lemmatize(word, pos='v') for word in n_lemmed]
+
+    return n_v_lemmed
 
 
 def build_model():
-    pass
+    '''Build machine learning model.
+
+    Count vectorize, Tfidf transform, and then classify with LinearSVC.
+    Added message length as an additional feature.
+    Grid search is applied.
+
+    Returns
+    -------
+    model: pipeline
+    '''
+
+    pipeline = Pipeline([
+        ('feature', FeatureUnion([
+            ('token', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+            ('length', Pipeline([
+                # custom transformer imported for using message length
+                ('extract', TextLengthExtractor()),
+
+                # min max scaling before using in classifier
+                ('scl', MinMaxScaler())
+            ]))
+        ])),
+        ('clf', MultiOutputClassifier(
+                OneVsRestClassifier(LinearSVC()), n_jobs=-1))
+    ])
+
+    # grid search parameters (minimumized for time constraints)
+    parameters = {
+        'feature__token__vect__ngram_range': [(1, 1)],
+        'feature__token__tfidf__use_idf': [True],
+        'clf__estimator__estimator__C': [1]
+    }
+
+    cv = GridSearchCV(pipeline, parameters, verbose=2)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''Evaluate model.
+
+    Predict output for test data.
+    Output f1 score, precision and recall for the test set for each category.
+
+    Parameters
+    ----------
+    model: pipeline
+        Evaluating model.
+
+    X_test: dataframe in shape = [n_samples, n_features]
+        The testing input samples.
+
+    Y_test: dataframe in shape = [n_samples, n_outputs]
+        The target values.
+
+    category_names: list in shape = [n_outputs,]
+        The column names of target values.
+    '''
+
+    Y_pred = model.predict(X_test)
+
+    for i, name in enumerate(category_names):
+        print(classification_report(Y_test.values[:, i],
+                                    Y_pred[:, i], target_names=[name]))
 
 
 def save_model(model, model_filepath):
-    pass
+    '''Save model to pickle file.
+
+    Parameters
+    ----------
+    model: pipeline
+        Evaluated model.
+
+    model_filepath: string
+        path to saving pickle file
+    '''
+
+    with open(model_filepath, mode='wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
@@ -27,13 +190,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
